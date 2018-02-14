adc2aa43c2efd157978e45251c316eeed8ddaff0
Steve Gehrman <sgehrman@gmail.com>
2018-02-14 02:57:14 -0800

    console component

--- src/App.vue
+++ src/App.vue
@@ -36,65 +36,8 @@
             <router-view></router-view>
           </keep-alive>
         </div>
-
-        <div class='app-console'>
-          <div class='console-bar'>
-            <v-btn icon dark @click.stop='fullscreenConsole()'>
-              <v-tooltip open-delay='800' bottom>
-                <v-icon v-if='fullscreenMode' slot='activator'>&#xE5D1;</v-icon>
-                <v-icon v-else slot='activator'>&#xE5D0;</v-icon>
-                <span>Full Screen</span>
-              </v-tooltip>
-            </v-btn>
-            <v-btn icon dark @click.stop='clearLog()'>
-              <v-tooltip open-delay='800' bottom>
-                <v-icon slot='activator'>&#xE15C;</v-icon>
-                <span>Clear console</span>
-              </v-tooltip>
-            </v-btn>
-            <div>
-              <v-menu offset-x :close-on-content-click="false" :nudge-width="200" v-model="menu">
-                <v-btn slot='activator' icon dark>
-                  <v-tooltip open-delay='800' bottom>
-                    <v-icon slot='activator'>&#xE8B8;</v-icon>
-                    <span>Console settings</span>
-                  </v-tooltip>
-                </v-btn>
-
-                <v-card>
-                  <v-list dense>
-                    <v-list-tile avatar>
-                      <v-list-tile-content>
-                        <v-list-tile-title>Console Settings</v-list-tile-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
-                  </v-list>
-                  <v-divider></v-divider>
-                  <v-list dense>
-                    <v-list-tile>
-                      <v-list-tile-action>
-                        <v-switch v-model="condensedOutput" color="primary"></v-switch>
-                      </v-list-tile-action>
-                      <v-list-tile-title>Condensed output</v-list-tile-title>
-                    </v-list-tile>
-                    <v-list-tile>
-                      <v-list-tile-action>
-                        <v-switch v-model="expandXDR" color="primary"></v-switch>
-                      </v-list-tile-action>
-                      <v-list-tile-title>Expand XDR</v-list-tile-title>
-                    </v-list-tile>
-                  </v-list>
-                </v-card>
-              </v-menu>
-            </div>
-          </div>
-
-          <textarea readonly="readonly" wrap="off" class='output-container' v-model='consoleOutput'></textarea>
-        </div>
       </div>
-
-      <!-- <footer-component publicKey='GBRKGAEJ7NLGCSF66Q5HZIG7OCKUOKF3N2POSDDGS2VPIWZQCA5HGP3V' /> -->
-      <!-- <footer-component publicKey='GCYQSB3UQDSISB5LKAL2OEVLAYJNIR7LFVYDNKRMLWQKDCBX4PU3Z6JP' /> -->
+      <console />
     </div>
 
     <toast-component />
@@ -106,6 +49,7 @@
 import NavigationDrawer from './components/NavigationDrawer.vue'
 import TickerComponent from './components/TickerComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
+import ConsoleComponent from './components/ConsoleComponent.vue'
 import NetworkMenu from './components/NetworkMenu.vue'
 import Helper from './js/helper.js'
 import Storage from './js/storage.js'
@@ -118,27 +62,15 @@ export default {
     'footer-component': FooterComponent,
     'navivation-drawer': NavigationDrawer,
     'network-menu': NetworkMenu,
-    'toast-component': ToastComponent
-  },
-  watch: {
-    condensedOutput: function () {
-      Helper.set('condensedOutput', this.condensedOutput)
-    },
-    expandXDR: function () {
-      Helper.set('expandXDR', this.expandXDR)
-    }
+    'toast-component': ToastComponent,
+    'console': ConsoleComponent
   },
   data() {
     return {
       showNavigation: false,
-      consoleOutput: '',
       snackbarText: '',
       snackbarModel: false,
       initializing: true,
-      menu: false,
-      condensedOutput: false,
-      expandXDR: false,
-      fullscreenMode: false,
       tabs: [{
         icon: String.fromCharCode('0xE88A'),
         tooltip: 'Make Payments',
@@ -182,9 +114,6 @@ export default {
         console.log(error)
       })
   },
-  mounted() {
-    Helper.vue().$on('console', this.log)
-  },
   methods: {
     fullscreenConsole() {
       const appconsole = $(this.$el).find('.app-console')
@@ -198,25 +127,6 @@ export default {
     },
     showDrawer() {
       this.showNavigation = !this.showNavigation
-    },
-    log(output) {
-      this.consoleOutput += output + '\n\n'
-
-      this.scrollToEnd()
-    },
-    scrollToEnd(delay = 250) {
-      this.$nextTick(() => {
-        $('.output-container').animate({
-          scrollTop: $('.output-container')[0].scrollHeight,
-          scrollLeft: 0
-        }, delay)
-      })
-    },
-    clearLog() {
-      this.consoleOutput = ''
-    },
-    consoleSettings() {
-      // sdfdf
     },
     toast(message) {
       this.snackbarText = message
@@ -322,40 +232,6 @@ html {
             left: 0;
             bottom: 0;
             right: 0;
-        }
-
-        .app-console {
-            position: relative;
-            display: flex;
-            flex: 1 1 80px;
-
-            .console-bar {
-                display: flex;
-                flex-direction: column;
-                position: absolute;
-                top: 0;
-                left: 0;
-
-                button {
-                    margin: 0;
-                    color: rgba(255,255,255,.4);
-                    &:hover {
-                        color: rgba(255,255,255,1);
-                    }
-                }
-            }
-
-            .output-container {
-                width: 100%;
-                resize: none;
-                outline: none;
-                font-size: 0.8em;
-                font-family: monospace;
-                background: rgb(0,20,0);
-                overflow: auto;
-                color: rgb(0,256,150);
-                padding: 10px;
-            }
         }
     }
 }
---
+++ src/components/ConsoleComponent.vue
@@ -0,0 +1,143 @@
+<template>
+<div class='app-console'>
+  <div class='console-bar'>
+    <v-btn icon dark @click.stop='fullscreenConsole()'>
+      <v-tooltip open-delay='800' bottom>
+        <v-icon v-if='fullscreenMode' slot='activator'>&#xE5D1;</v-icon>
+        <v-icon v-else slot='activator'>&#xE5D0;</v-icon>
+        <span>Full Screen</span>
+      </v-tooltip>
+    </v-btn>
+    <v-btn icon dark @click.stop='clearLog()'>
+      <v-tooltip open-delay='800' bottom>
+        <v-icon slot='activator'>&#xE15C;</v-icon>
+        <span>Clear console</span>
+      </v-tooltip>
+    </v-btn>
+    <div>
+      <v-menu offset-x :close-on-content-click="false" :nudge-width="200" v-model="menu">
+        <v-btn slot='activator' icon dark>
+          <v-tooltip open-delay='800' bottom>
+            <v-icon slot='activator'>&#xE8B8;</v-icon>
+            <span>Console settings</span>
+          </v-tooltip>
+        </v-btn>
+
+        <v-card>
+          <v-list dense>
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>Console Settings</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-divider></v-divider>
+          <v-list dense>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="condensedOutput" color="primary"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Condensed output</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="expandXDR" color="primary"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Expand XDR</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </div>
+  </div>
+
+  <textarea readonly="readonly" wrap="off" class='output-container' v-model='consoleOutput'></textarea>
+</div>
+</template>
+
+<script>
+import BalanceComponent from './BalanceComponent.vue'
+import Helper from '../js/helper.js'
+import $ from 'jquery'
+
+export default {
+  props: ['publicKey'],
+  components: {
+    'balance-component': BalanceComponent
+  },
+  watch: {
+    condensedOutput: function () {
+      Helper.set('condensedOutput', this.condensedOutput)
+    },
+    expandXDR: function () {
+      Helper.set('expandXDR', this.expandXDR)
+    }
+  },
+  data() {
+    return {
+      consoleOutput: '',
+      menu: false,
+      condensedOutput: false,
+      expandXDR: false,
+      fullscreenMode: false
+    }
+  },
+  mounted() {
+    Helper.vue().$on('console', this.log)
+  },
+  methods: {
+    log(output) {
+      this.consoleOutput += output + '\n\n'
+
+      this.scrollToEnd()
+    },
+    scrollToEnd(delay = 250) {
+      this.$nextTick(() => {
+        $('.output-container').animate({
+          scrollTop: $('.output-container')[0].scrollHeight,
+          scrollLeft: 0
+        }, delay)
+      })
+    },
+    clearLog() {
+      this.consoleOutput = ''
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.app-console {
+    position: relative;
+    display: flex;
+    flex: 1 1 80px;
+
+    .console-bar {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        button {
+            margin: 0;
+            color: rgba(255,255,255,.4);
+            &:hover {
+                color: rgba(255,255,255,1);
+            }
+        }
+    }
+
+    .output-container {
+        width: 100%;
+        resize: none;
+        outline: none;
+        font-size: 0.8em;
+        font-family: monospace;
+        background: rgb(0,20,0);
+        overflow: auto;
+        color: rgb(0,256,150);
+        padding: 10px;
+    }
+}
+</style>
