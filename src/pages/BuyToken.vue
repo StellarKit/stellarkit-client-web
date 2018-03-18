70f4d4d3630b268abc151473a8122db91666dc30
Steve Gehrman <sgehrman@gmail.com>
2018-03-17 17:11:05 -0700

    sdf

--- src/pages/BuyToken.vue
+++ src/pages/BuyToken.vue
@@ -5,9 +5,21 @@
   </instructions-header>
 
   <div class='main-container'>
-    <div>This is only going to work if you have setup a local server with Bifrost, BTC and ETH servers configured. </div>
-    <div>See <a href="https://github.com/StellarKit" target="_blank">https://github.com/StellarKit</a> for docker images to test on testnet.</div>
-    <v-btn round small @click="showDialog">Buy Token</v-btn>
+    <div class='bifrost-notes'>
+      <div>This is only going to work if you have setup a local server with Bifrost, BTC and ETH servers configured. </div>
+      <div>See <a href="https://github.com/StellarKit" target="_blank">https://github.com/StellarKit</a> for docker images to test on testnet.</div>
+    </div>
+
+    <div class='bifrost-params'>
+      <v-text-field hide-details label="Price" type='number' v-model.number="price"></v-text-field>
+      <v-text-field hide-details label="Asset code" v-model.trim="assetCode"></v-text-field>
+      <v-text-field hide-details label="Asset issuer" v-model.trim="assetIssuer"></v-text-field>
+      <v-text-field hide-details label="Horizon IP Address" v-model.trim="horizonIP"></v-text-field>
+      <v-text-field hide-details label="Bifrost IP Address" v-model.trim="biforstIP"></v-text-field>
+      <v-text-field hide-details label="Network" v-model.trim="network"></v-text-field>
+    </div>
+
+    <v-btn round small color='primary' @click="showDialog">Buy Token</v-btn>
 
     <buy-token-dialog :ping='showDialogPing' :params='params' :allowHTTP='allowHTTP' />
   </div>
@@ -29,17 +41,29 @@ export default {
   data() {
     return {
       showDialogPing: false,
-      params: null
+      params: null,
+      price: 1,
+      assetCode: 'LMB',
+      assetIssuer: '',
+      horizonIP: '192.168.1.82:8000',
+      biforstIP: '192.168.1.82:8800',
+      network: 'test'
     }
   },
   computed: {
-    allowHTTP: function () {
+    allowHTTP: function() {
       return window.location.protocol !== 'https:'
     }
   },
   components: {
     'buy-token-dialog': BuyTokenDialog,
     'instructions-header': InstructionsHeader
+  },
+  mounted() {
+    const issuerAcct = StellarAccounts.accountWithName('Issuer')
+    if (issuerAcct) {
+      this.assetIssuer = issuerAcct.publicKey
+    }
   },
   methods: {
     showDialog() {
@@ -48,12 +72,12 @@ export default {
       const issuerAcct = StellarAccounts.accountWithName('Issuer')
       if (issuerAcct) {
         this.params = {
-          network: 'test',
-          horizonURL: protocol + '//192.168.1.82:8000',
-          bifrostURL: protocol + '//192.168.1.82:8800',
-          assetCode: 'LMB',
-          price: '1',
-          issuingPublicKey: issuerAcct.publicKey,
+          network: this.network,
+          horizonURL: protocol + '//' + this.horizonIP,
+          bifrostURL: protocol + '//' + this.biforstIP,
+          assetCode: this.assetCode,
+          price: String(this.price),
+          issuingPublicKey: this.assetIssuer,
           preSaleMode: false
         }
 
@@ -75,7 +99,22 @@ export default {
     align-items:: center;
 
     button {
-        margin: 40px;
+        margin: 20px;
+    }
+
+    .bifrost-notes {
+        margin-bottom: 20px;
+    }
+
+    .bifrost-params {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        .input-group {
+            margin-right: 12px;
+            flex: 1 0 200px;
+        }
     }
 }
 </style>
