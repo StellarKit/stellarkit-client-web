7d114e1fb21c96673ffac616fcb116cc3bcc644b
Steve Gehrman <sgehrman@gmail.com>
2018-03-31 02:03:15 -0700

    sf

--- src/components/AssetPopup.vue
+++ src/components/AssetPopup.vue
@@ -5,18 +5,19 @@
   </div>
   <v-menu offset-y>
     <v-btn flat color='primary' class='custom-menu-button' slot="activator">
-      {{getTitle()}}
+      <div>{{getTitle()}}</div>
       <v-icon>&#xE5C5;</v-icon>
     </v-btn>
     <v-list dense>
       <v-list-tile v-for="asset of assets" :key="asset.symbol + asset.issuer" @click="menuClick(asset)">
         <div>{{asset.symbol}}</div>
       </v-list-tile>
-      <v-list-tile @click="assetDialogPing = !assetDialogPing">
+      <v-list-tile class='edit-assets-item' @click="assetDialogPing = !assetDialogPing">
         <div>Edit Assets...</div>
       </v-list-tile>
     </v-list>
   </v-menu>
+  <div class='button-issuer'>{{getIssuer()}}</div>
 
   <assets-dialog :ping='assetDialogPing' />
 </div>
@@ -64,6 +65,13 @@ export default {
 
       return ''
     },
+    getIssuer() {
+      if (this.selectedItem) {
+        return this.selectedItem.issuer
+      }
+
+      return ''
+    },
     menuClick(item) {
       this.selectedItem = item
     },
@@ -90,5 +98,18 @@ export default {
     .custom-menu-button {
         margin: 0 0 0 6px;
     }
+
+    .button-issuer {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        font-size: 0.8em;
+        color: rgba(0,0,0,.4);
+        max-width: 200px;
+    }
+}
+
+.edit-assets-item {
+    color: steelblue;
 }
 </style>
