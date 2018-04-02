1a88f26cd8545472d7f3cb48f6c7d68eee8b0deb
Steve Gehrman <sgehrman@gmail.com>
2018-04-02 04:39:26 -0700

    sdf

--- src/components/dialogs/NewAccountDialog.vue
+++ src/components/dialogs/NewAccountDialog.vue
@@ -27,7 +27,7 @@
         </div>
 
         <div v-if='mode === "secret"' class='choice-box'>
-          <div class='note-text'>Choose an account to fund the creating of a new account. <strong>Use either</strong> an account on Stellar Army, or paste in a secret key from another account. 1 XLM will be spent to fund the new account.</div>
+          <div class='note-text'>Choose an account to fund the creating of a new account. <strong>Use either</strong> an account on Stellar Army, or paste in a secret key from another account.</div>
           <dialog-accounts ref='dialogAccounts' v-on:enter-key-down='createAccount' v-on:toast='displayToast' :model="model" :showFunding=true :showAccountName=true :showAmount=true />
           <div class='button-holder'>
             <v-tooltip open-delay='200' bottom>
