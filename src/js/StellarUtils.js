e46cdefa89b1a17f6aee24c9688620590c49833c
Steve Gehrman <sgehrman@gmail.com>
2018-04-02 18:02:58 -0700

    sdf

--- src/js/StellarUtils.js
+++ src/js/StellarUtils.js
@@ -237,7 +237,7 @@ class StellarUtils {
             }
           })
       })
-      .then((result) => {
+      .then(() => {
         Helper.debugLog('sending tokens...')
         return this.sendAsset(sourceWallet, fundingWallet, newWallet, amount, asset)
       })
