4ddd8a251e0b115cd8d983d1d0c3034f3c3c5405
Steve Gehrman <sgehrman@gmail.com>
2019-04-01 15:14:23 -0700

    testing prettier

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -1,55 +1,29 @@
 <template>
-<v-navigation-drawer
-  class='navigation-drawer'
-  absolute
-  clipped
-  temporary
-  v-model="drawer"
->
-  <div>
-    <div
-      class='list-header-item'
-      @click="clickItem()"
-    >
-      <div class="title">
-        {{applicationName}}
+  <v-navigation-drawer class="navigation-drawer" absolute clipped temporary v-model="drawer">
+    <div>
+      <div class="list-header-item" @click="clickItem()">
+        <div class="title">{{applicationName}}</div>
       </div>
     </div>
-  </div>
-  <v-divider></v-divider>
-  <v-list>
-    <v-list-tile
-      v-for="item in items"
-      :key="item.title"
-      @click="clickItem(item)"
-    >
-      <div class='list-item-icon'>
-        <v-icon>{{item.icon}}</v-icon>
-      </div>
-      <v-list-tile-content>
-        <div class='list-item-text'>{{item.title}}</div>
-      </v-list-tile-content>
-    </v-list-tile>
-  </v-list>
-  <donate-dialog
-    :ping='showDonatePing'
-    :donationPublicKey='donationPublicKey'
-  />
-  <help-dialog
-    :ping='showHelpPing'
-    title='Help'
-  />
-  <about-dialog
-    :ping='showAboutPing'
-    title='About'
-  />
-</v-navigation-drawer>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-tile v-for="item in items" :key="item.title" @click="clickItem(item)">
+        <div class="list-item-icon">
+          <v-icon>{{item.icon}}</v-icon>
+        </div>
+        <v-list-tile-content>
+          <div class="list-item-text">{{item.title}}</div>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <donate-dialog :ping="showDonatePing" :donationPublicKey="donationPublicKey"/>
+    <help-dialog :ping="showHelpPing" title="Help"/>
+    <about-dialog :ping="showAboutPing" title="About"/>
+  </v-navigation-drawer>
 </template>
 
 <script>
-import {
-  PaymentDialog
-} from 'stellarkit-js-ui'
+import { PaymentDialog } from 'stellarkit-js-ui'
 import Helper from '../js/helper.js'
 import ExpansionDialog from './dialogs/ExpansionDialog.vue'
 import AboutDialog from './dialogs/AboutDialog.vue'
@@ -59,17 +33,17 @@ export default {
   components: {
     'donate-dialog': PaymentDialog,
     'help-dialog': ExpansionDialog,
-    'about-dialog': AboutDialog
+    'about-dialog': AboutDialog,
   },
   watch: {
     ping: function() {
       this.drawer = true
-    }
+    },
   },
   computed: {
     applicationName: function() {
       return Helper.applicationName()
-    }
+    },
   },
   data() {
     return {
@@ -77,28 +51,30 @@ export default {
       showAboutPing: false,
       showDonatePing: false,
       drawer: false,
-      donationPublicKey: 'GBJC6AF4I5FUTYMG4CXC3V2NYMIQANBRB4UQYY3M2RRZCXCNLFR7TN7J',
-      items: [{
+      donationPublicKey:
+        'GBJC6AF4I5FUTYMG4CXC3V2NYMIQANBRB4UQYY3M2RRZCXCNLFR7TN7J',
+      items: [
+        {
           id: 'about',
           title: 'About',
-          icon: 'person'
+          icon: 'person',
         },
         {
           id: 'donate',
           title: 'Donate',
-          icon: 'local_atm'
+          icon: 'local_atm',
         },
         {
           id: 'help',
           title: 'Help',
-          icon: 'help'
+          icon: 'help',
         },
         {
           id: 'github',
           title: 'Source on Github',
-          icon: 'fab fa-github'
-        }
-      ]
+          icon: 'fab fa-github',
+        },
+      ],
     }
   },
   methods: {
@@ -115,7 +91,9 @@ export default {
             this.showAboutPing = !this.showAboutPing
             break
           case 'github':
-            Helper.openBrowser('https://github.com/StellarKit/stellarkit-client-web')
+            Helper.openBrowser(
+              'https://github.com/StellarKit/stellarkit-client-web'
+            )
             break
           default:
             break
@@ -124,28 +102,28 @@ export default {
 
       // hide drawer for every click
       this.drawer = false
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style lang='scss' scoped>
 .list-header-item {
-    display: flex;
-    align-items: center;
-    padding: 12px;
-    color: white;
-    background: steelblue;
+  display: flex;
+  align-items: center;
+  padding: 12px;
+  color: white;
+  background: steelblue;
 }
 
 .navigation-drawer {
-    background: rgb(245,245,245);
+  background: rgb(245, 245, 245);
 }
 .list-item-text {
-    font-size: 1.3em;
-    color: rgba(0,0,0,.6);
+  font-size: 1.3em;
+  color: rgba(0, 0, 0, 0.6);
 }
 .list-item-icon {
-    margin-right: 16px;
+  margin-right: 16px;
 }
 </style>
